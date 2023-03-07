e95e333d3e4caefc1638a9de3e2b1dff207be46f
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-03-08 00:24:25 +0530

    code: G27

--- G27 - Shortest Path in a DAG using Topological Sort/usingTopoSort.cpp
+++ G27 - Shortest Path in a DAG using Topological Sort/usingTopoSort.cpp
@@ -1,4 +1,10 @@
+// Given a DAG, find the shortest path from the source to all other nodes in this DAG. In this problem statement, we have assumed the source vertex to be ‘0’. You will be given the weighted edges of the graph.
 
+// Note: What is a DAG ( Directed Acyclic Graph)?
+
+// A Directed Graph (containing one-sided edges) having no cycles is said to be a Directed Acyclic Graph.
+
+// StackOverFlow - https://stackoverflow.com/questions/68449275/why-do-we-need-to-perform-topological-ordering-first-to-find-shortest-path-in-a
 #include <bits/stdc++.h>
 using namespace std;
 
