e24d372c59c5dd7307f546fb1cc16c0d27c53036
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-03-19 18:45:15 +0530

    G29 added

--- G29 - Word Ladder - I/bfs.cpp
+++ G29 - Word Ladder - I/bfs.cpp
@@ -27,32 +27,35 @@ public:
        
        while(!q.empty()){
            
-           string word = q.front().first;
-           int steps = q.front().second;
-           q.pop();
-           
-           if(word == targetWord) return steps;
-           
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            // we return the steps as soon as
+            // the first occurence of targetWord is found.
+            if(word == targetWord) return steps;
+            
 
-           // N * wordLength * 26
-           for(int i=0 ; i<word.size() ; i++){
-               
-               char original = word[i];
-               
-               for(char ch = 'a' ; ch<='z' ; ch++){
-                   word[i] = ch; 
-                   
-                   if(st.find(word) != st.end()){
-                       st.erase(word);
-                       q.push({word , steps + 1});
-                   }
-               }
-               
-               word[i] = original;
-               
-           }
+            // N * wordLength * 26
+            for(int i=0 ; i<word.size() ; i++){
+                // Now, replace each character of ‘word’ with char
+                // from a-z then check if ‘word’ exists in wordList.
+                char original = word[i];
+                
+                for(char ch = 'a' ; ch<='z' ; ch++){
+                    word[i] = ch; 
+                    // check if it exists in the set and push it in the queue.
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        q.push({word , steps + 1});
+                    }
+                }
+                
+                word[i] = original;
+                
+            }
        }
-       
+       // If there is no transformation sequence possible
        return 0;
     }
 };
