c5c79e78888e4ce74a9b142412126e14fa225078
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-03-19 18:42:56 +0530

    G29 added

--- G28 - Shortest Path in an Undirected Graph having unit weights/usedBFS.cpp
+++ G28 - Shortest Path in an Undirected Graph having unit weights/usedBFS.cpp
@@ -1,4 +1,4 @@
-
+// You are given an Undirected Graph having unit weight, Find the shortest path from src to all the vertex and if it is unreachable to reach any vertex, then return -1 for that vertex.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,8 +8,10 @@ using namespace std;
 class Solution {
   public:
     vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        //Create an adjacency list of size N for storing the undirected graph.
         vector<int> adj[N];
         
+        //undirected meaning u ---> v && v ---> u
         for(int i = 0; i < M; i++){
             int u = edges[i][0];
             int v = edges[i][1];
@@ -17,10 +19,13 @@ class Solution {
             adj[v].push_back(u);
         }
         
+        //A dist array of size N initialised with a large number to 
+        //indicate that initially all the nodes are untraversed.
         vector<int> dist(N,INT_MAX);
         
         dist[src] = 0;
         
+        //BFS Implementation
         queue<pair<int,int>> q;
         q.push({src,0});
         
@@ -37,6 +42,8 @@ class Solution {
             }
         }
         
+        // Updated shortest distances are stored in the resultant array ‘dist’.
+        // Unreachable nodes are marked as -1. 
         for(int i = 0; i < N; i++){
             if(dist[i] == INT_MAX) dist[i] = -1; 
         }
---
+++ G29 - Word Ladder - I/bfs.cpp
@@ -0,0 +1,75 @@
+/*Given two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find the length of the shortest transformation sequence from startWord to targetWord.
+Keep the following conditions in mind:
+
+A word can only consist of lowercase characters.
+Only one letter can be changed in each transformation.
+Each transformed word must exist in the wordList including the targetWord.
+startWord may or may not be part of the wordList
+The second part of this problem can be found here.
+
+Note: If no possible way to transform sequence from startWord to targetWord return 0
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
+       
+       queue<pair<string , int>>q;  
+       q.push({startWord , 1});
+       
+       //set for checking words effectively - O(N) space
+       unordered_set<string>st(wordList.begin() , wordList.end());
+       st.erase(startWord);
+       
+       while(!q.empty()){
+           
+           string word = q.front().first;
+           int steps = q.front().second;
+           q.pop();
+           
+           if(word == targetWord) return steps;
+           
+
+           // N * wordLength * 26
+           for(int i=0 ; i<word.size() ; i++){
+               
+               char original = word[i];
+               
+               for(char ch = 'a' ; ch<='z' ; ch++){
+                   word[i] = ch; 
+                   
+                   if(st.find(word) != st.end()){
+                       st.erase(word);
+                       q.push({word , steps + 1});
+                   }
+               }
+               
+               word[i] = original;
+               
+           }
+       }
+       
+       return 0;
+    }
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+		cout << ans << "\n";
+	}
+	return 0;
+}
