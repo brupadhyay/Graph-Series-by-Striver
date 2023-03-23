f9cbe4d05db9a33089fa4fb4a062af3a1c7d5141
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-03-23 22:35:07 +0530

    G35 Added

---
+++ G35 - Print Shortest Path in Weighted  Undirected Graph/printUsingDijkstras.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> shortestPath(int n, int m, vector<vector<int>> &edges)
+    {
+        // Create an adjacency list of pairs of the form node1 -> {node2, edge weight}
+        // where the edge weight is the weight of the edge from node1 to node2.
+        vector<pair<int, int>> adj[n + 1];
+        for (auto it : edges)
+        {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        // Create a priority queue for storing the nodes along with distances
+        // in the form of a pair { dist, node }.
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        // Create a dist array for storing the updated distances and a parent array
+        // for storing the nodes from where the current nodes represented by indices of
+        // the parent array came from.
+        vector<int> dist(n + 1, 1e9), parent(n + 1);
+        for (int i = 1; i <= n; i++)
+            parent[i] = i;
+
+        dist[1] = 0;
+
+        // Push the source node to the queue.
+        pq.push({0, 1});
+        while (!pq.empty())
+        {
+            // Topmost element of the priority queue is with minimum distance value.
+            auto it = pq.top();
+            pq.pop();
+            int node = it.second;
+            int dis = it.first;
+
+            // Iterate through the adjacent nodes of the current popped node.
+            for (auto it : adj[node])
+            {
+                int adjNode = it.first;
+                int edW = it.second;
+
+                // Check if the previously stored distance value is
+                // greater than the current computed value or not,
+                // if yes then update the distance value.
+                if (dis + edW < dist[adjNode])
+                {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+
+                    // Update the parent of the adjNode to the recent
+                    // node where it came from.
+                    parent[adjNode] = node;
+                }
+            }
+        }
+
+        // If distance to a node could not be found, return an array containing -1.
+        if (dist[n] == 1e9)
+            return {-1};
+
+        // Store the final path in the ‘path’ array.
+        vector<int> path;
+        int node = n;
+
+        // Iterate backwards from destination to source through the parent array.
+        while (parent[node] != node)
+        {
+            path.push_back(node);
+            node = parent[node];
+        }
+        path.push_back(1);
+
+        // Since the path stored is in a reverse order, we reverse the array
+        // to get the final answer and then return the array.
+        reverse(path.begin(), path.end());
+        return path;
+    }
+};
+
+int main()
+{
+    // Driver Code
+
+    int V = 5, E = 6;
+    vector<vector<int>> edges = {{1, 2, 2}, {2, 5, 5}, {2, 3, 4}, {1, 4, 1}, {4, 3, 3}, {3, 5, 1}};
+    Solution obj;
+    vector<int> path = obj.shortestPath(V, E, edges);
+
+    for (int i = 0; i < path.size(); i++)
+    {
+
+        cout << path[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
