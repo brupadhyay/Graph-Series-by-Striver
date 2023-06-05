7370bc1d1f4f77229e0c5ec7a558cb8e305335a0
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-06-05 18:43:33 +0530

    CODE: Number of Provinces

---
+++ G48 - Number of Provinces/usingDSU.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        size.resize(n, 1);
+        parent.resize(n);
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findPar(int u)
+    {
+        if (u == parent[u])
+            return u;
+        return parent[u] = findPar(parent[u]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findPar(u);
+        int ulp_v = findPar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] > size[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+        else
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+class Solution
+{
+public:
+    int numProvinces(vector<vector<int>> adj, int V)
+    {
+        DisjointSet ds(V);
+
+        // V x V x 4 x alpha
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (adj[i][j] == 1)
+                {
+                    ds.unionBySize(i, j);
+                }
+            }
+        }
+
+        int numberOfProvinces = 0;
+
+        // V x 4 x alpha
+        for (int i = 0; i < V; i++)
+        {
+            if (ds.findPar(i) == i)
+                numberOfProvinces++;
+        }
+        return numberOfProvinces;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, x;
+        cin >> V;
+
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < V; i++)
+        {
+            vector<int> temp;
+            for (int j = 0; j < V; j++)
+            {
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution ob;
+        cout << ob.numProvinces(adj, V) << endl;
+    }
+    return 0;
+}
